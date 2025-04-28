37297f3b81f33f30d9e84e3e811990fb58ebea13
wmayer <wmayer@freecad.org>
2025-04-28 18:57:36 +0200

    Part: Implement PartPseudoShapeProvider
    Add the interface class PseudoShapeProvider and implement
    PartPseudoShapeProvider to avoid to relying on implementation
    details of an extension module in the core system.

--- src/App/Services.h
+++ src/App/Services.h
@@ -81,4 +81,15 @@ public:
     virtual std::optional<PyObject*> getAttribute(DocumentObject* object, const char* attr) const = 0;
 };
 
+/**
+* This service should provide access to shape elements
+*/
+class PseudoShapeProvider
+{
+public:
+    virtual ~PseudoShapeProvider() = default;
+
+    virtual Py::Object getElement(const Py::Object& module, const Py::Object& object, const std::string& subname) const = 0;
+};
+
 }
