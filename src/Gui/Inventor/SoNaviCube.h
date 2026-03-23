c2c2a21eed8bfbfe10c60f5fd3d17a925fd3bca1
Chris Hennes <chennes@pioneerlibrarysystem.org>
2026-03-22 19:37:25 -0500

    Gui/Inventor: Update copyright and guards on new file

--- src/Gui/Inventor/SoNaviCube.h
+++ src/Gui/Inventor/SoNaviCube.h
@@ -1,28 +1,26 @@
-/***************************************************************************
- *   Copyright (c) 2017 Kustaa Nyholm  <kustaa.nyholm@sparetimelabs.com>   *
- *   Copyright (c) 2025 Joao Matos                                         *
- *                                                                         *
- *   This file is part of the FreeCAD CAx development system.              *
- *                                                                         *
- *   This library is free software; you can redistribute it and/or         *
- *   modify it under the terms of the GNU Library General Public           *
- *   License as published by the Free Software Foundation; either          *
- *   version 2 of the License, or (at your option) any later version.      *
- *                                                                         *
- *   This library  is distributed in the hope that it will be useful,      *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU Library General Public License for more details.                  *
- *                                                                         *
- *   You should have received a copy of the GNU Library General Public     *
- *   License along with this library; see the file COPYING.LIB. If not,    *
- *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
- *   Suite 330, Boston, MA  02111-1307, USA                                *
- *                                                                         *
- ***************************************************************************/
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// SPDX-FileCopyrightText: 2017 Kustaa Nyholm <kustaa.nyholm@sparetimelabs.com>
+// SPDX-FileCopyrightText: 2025 Joao Matos
+// SPDX-FileNotice: Part of the FreeCAD project.
 
-#ifndef GUI_INVENTOR_SONAVICUBE_H
-#define GUI_INVENTOR_SONAVICUBE_H
+/******************************************************************************
+ *                                                                            *
+ *   FreeCAD is free software: you can redistribute it and/or modify          *
+ *   it under the terms of the GNU Lesser General Public License as           *
+ *   published by the Free Software Foundation, either version 2.1            *
+ *   of the License, or (at your option) any later version.                   *
+ *                                                                            *
+ *   FreeCAD is distributed in the hope that it will be useful,               *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty              *
+ *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *
+ *   See the GNU Lesser General Public License for more details.              *
+ *                                                                            *
+ *   You should have received a copy of the GNU Lesser General Public         *
+ *   License along with FreeCAD. If not, see https://www.gnu.org/licenses     *
+ *                                                                            *
+ ******************************************************************************/
+
+#pragma once
 
 #include <FCGlobal.h>
 #include <Inventor/SbColor.h>
@@ -186,5 +184,3 @@ public:
 };
 
 }  // namespace Gui
-
-#endif  // GUI_INVENTOR_SONAVICUBE_H
